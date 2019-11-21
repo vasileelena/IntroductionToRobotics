4b913185f99596ec343c0c338b6c61f593296e0d
vasileelena <57196944+vasileelena@users.noreply.github.com>
2019-11-21 19:11:57 +0200

    Update 4digits7segmentsJoystick.ino

--- LabHomeworks/4digits7segmentsJoystick.ino
+++ LabHomeworks/4digits7segmentsJoystick.ino
@@ -15,157 +15,10 @@ const int segSize = 8;
 const int noOfDisplays = 4;
 const int noOfDigits = 10;
 
-int stateDP = LOW;
-int displayNo = 1;
+int prevButtonValue = 1;
+int lastDebounceTime = 0;
+int debounceDelay = 50;
 
-const int segments[segSize] = {pinA, pinB, pinC, pinD, pinE, pinF, pinG, pinDP};
-const int digits[noOfDisplays] = {pinD1, pinD2, pinD3, pinD4};
-const byte digitMatrix[noOfDigits][segSize - 1] = {
-// a  b  c  d  e  f  g
-  {1, 1, 1, 1, 1, 1, 0}, // 0
-  {0, 1, 1, 0, 0, 0, 0}, // 1
-  {1, 1, 0, 1, 1, 0, 1}, // 2
-  {1, 1, 1, 1, 0, 0, 1}, // 3
-  {0, 1, 1, 0, 0, 1, 1}, // 4
-  {1, 0, 1, 1, 0, 1, 1}, // 5
-  {1, 0, 1, 1, 1, 1, 1}, // 6
-  {1, 1, 1, 0, 0, 0, 0}, // 7
-  {1, 1, 1, 1, 1, 1, 1}, // 8
-  {1, 1, 1, 1, 0, 1, 1}  // 9
-};
-int values[4] = {0, 0, 0, 0};
-int dots[4] = {0, 0, 0, 0};
-
-const int interval = 500;
-int lastBlink = 0;
-
-const int pinSW = A2;
-const int pinX = A0;
-const int pinY = A1;
-
-int switchValue;
-int xValue = 0;
-int yValue = 0;
-int xMinThreshold = 500;
-int xMaxThreshold = 550;
-int yMinThreshold = 400;
-int yMaxThreshold = 700;
-bool xMoved = 0;
-bool yMoved = 0;
-
-void displayFunction(byte digit, byte decimalPoint){
-  for(int i = 0; i < segSize - 1; i++){
-    digitalWrite(segments[i], digitMatrix[digit][i]);
-  }
- digitalWrite(segments[segSize - 1], decimalPoint);
-}
-
-void showDigit(int num){
-  for(int i = 0; i < noOfDisplays; i++){
-    digitalWrite(digits[i], HIGH);
-  }
-  digitalWrite(digits[num-1], LOW);
-}
-
-void setup() {
-  for(int i = 0; i < segSize ; i++){
-    pinMode(segments[i], OUTPUT);
-  }
-  for(int i = 0; i< noOfDisplays; i++){
-    pinMode(digits[i], OUTPUT);
-  }
-  pinMode(pinSW, INPUT_PULLUP);
-  Serial.begin(9600);
-}
-
-int buttonCount = 0;
-
-void loop() {
-  
-  switchValue = digitalRead(pinSW);
-  xValue = analogRead(pinX);
-  yValue = analogRead(pinY);
-
-
-if(buttonCount % 2 == 0){
-  if(yValue > yMaxThreshold && !yMoved){
-    dots[displayNo - 1] = LOW;
-    displayNo--;
-    if(displayNo < 1)
-      displayNo = 4;
-    yMoved = 1;
-    //buttonState = 0;
-  }
-  
-  if(yValue < yMinThreshold && !yMoved){
-    dots[displayNo - 1] = LOW;
-    displayNo++;
-    if(displayNo > 4)
-      displayNo = 1;
-    yMoved = 1;  
-    //buttonState = 0;
-  }
-
-   if(yValue > yMinThreshold && yValue < yMaxThreshold){
-    yMoved = 0;
-    if(millis() - lastBlink >= interval){
-      dots[displayNo - 1] = !dots[displayNo - 1];
-      lastBlink = millis();
-    }
-   }
-}
-  if(switchValue == LOW){
-     buttonCount++;
-  }
-   if(buttonCount % 2 == 1){
-    dots[displayNo - 1] = HIGH;
-  if(xValue < xMinThreshold && !xMoved){
-    values[displayNo - 1]--;
-    if(values[displayNo - 1] < 0)
-      values[displayNo - 1] = 9;
-    xMoved = 1;
-  }
-  if(xValue > xMaxThreshold && !xMoved){
-    values[displayNo - 1]++;
-    if(values[displayNo - 1] > 9)
-      values[displayNo - 1] = 0;
-    xMoved = 1;
-  }
-   if(xValue > xMinThreshold && xValue < xMaxThreshold){
-    xMoved = 0;
-  }
-  
- }
-  showDigit(1);
-  displayFunction(values[0], dots[0]);
-  delay(5);
-  showDigit(2);
-  displayFunction(values[1], dots[1]);
-  delay(5);
-  showDigit(3);
-  displayFunction(values[2], dots[2]);
-  delay(5);
-  showDigit(4);
-  displayFunction(values[3], dots[3]);
-  delay(5);
-  
-
-}const int pinA = 12;
-const int pinB = 8;
-const int pinC = 5;
-const int pinD = 3;
-const int pinE = 2;
-const int pinF = 11;
-const int pinG = 6;
-const int pinDP = 4;
-const int pinD1 = 7;
-const int pinD2 = 9;
-const int pinD3 = 10;
-const int pinD4 = 13;
-
-const int segSize = 8;
-const int noOfDisplays = 4;
-const int noOfDigits = 10;
 
 int stateDP = LOW;
 int displayNo = 1;
@@ -230,7 +83,7 @@ void setup() {
   Serial.begin(9600);
 }
 
-int buttonCount = 0;
+int buttonState = 0;
 
 void loop() {
   
@@ -239,14 +92,13 @@ void loop() {
   yValue = analogRead(pinY);
 
 
-if(buttonCount % 2 == 0){
+if(buttonState == 0){
   if(yValue > yMaxThreshold && !yMoved){
     dots[displayNo - 1] = LOW;
     displayNo--;
     if(displayNo < 1)
       displayNo = 4;
     yMoved = 1;
-    //buttonState = 0;
   }
   
   if(yValue < yMinThreshold && !yMoved){
@@ -255,7 +107,6 @@ if(buttonCount % 2 == 0){
     if(displayNo > 4)
       displayNo = 1;
     yMoved = 1;  
-    //buttonState = 0;
   }
 
    if(yValue > yMinThreshold && yValue < yMaxThreshold){
@@ -266,10 +117,15 @@ if(buttonCount % 2 == 0){
     }
    }
 }
-  if(switchValue == LOW){
-     buttonCount++;
+
+  if(prevButtonValue != switchValue){
+     if(switchValue == 0){
+       buttonState = !buttonState;
+     }
   }
-   if(buttonCount % 2 == 1){
+  prevButtonValue = switchValue;
+  
+   if(buttonState == 1){
     dots[displayNo - 1] = HIGH;
   if(xValue < xMinThreshold && !xMoved){
     values[displayNo - 1]--;
